531cfbf1c81b5da3059c28f3e5a28449af2d8d4c
Karsten Müller <karsten.mueller@lichtblick.de>
2019-01-18 14:59:19 +0100

    changed to yujitach-menumeters

--- Casks/menumeters.rb
+++ Casks/menumeters.rb
@@ -1,14 +1,12 @@
 cask 'menumeters' do
-  version :latest
-  sha256 :no_check
-
-  url 'https://www.ragingmenace.com/software/download/MenuMeters.dmg'
-  name 'MenuMeters'
-  homepage 'https://www.ragingmenace.com/software/menumeters/'
-
-  depends_on macos: '<= :yosemite'
-
-  prefpane 'MenuMeters Installer.app/Contents/Resources/MenuMeters.prefPane'
-
+  version '1.9.7bis'
+  sha256 '6cf2f7b312c1c13f74e9d56082f48b50b7ca5909ee3589df916a65818ac390d9'
+# github.com/yujitach/MenuMeters was verified as official when first introduced to the cask
+  url "https://github.com/yujitach/MenuMeters/releases/download/#{version}/MenuMeters_#{version.major_minor_patch}.zip"
+  appcast 'https://github.com/yujitach/MenuMeters/releases.atom'
+  name 'MenuMeters for El Capitan (and later)'
+  homepage 'https://member.ipmu.jp/yuji.tachikawa/MenuMetersElCapitan/'
+  depends_on macos: '>= :el_capitan'
+  prefpane 'MenuMeters.prefPane'
   zap trash: '~/Library/Preferences/com.ragingmenace.MenuMeters.plist'
 end
